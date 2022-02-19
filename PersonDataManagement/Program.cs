b19487c7db0d5eaf50928491eee4849dc44b5407
Guruprasad489 <guruprasadk.dev@gmail.com>
2022-02-20 00:52:58 +0530

    [Guru] Added method to get persons age between 13 to 18 -UC3

--- PersonDataManagement/Program.cs
+++ PersonDataManagement/Program.cs
@@ -13,8 +13,22 @@ namespace PersonDataManagement
             Console.WriteLine("Welcome to Person data management program");
             List<Person> list = new List<Person>();
             AddPersonDetailsToList(list);
-            RetrievePersonAgeLessThan60(list);
 
+            Console.WriteLine("Choose an option \n1. Retrieve top 2 Person Age Less Than 60" +
+                                               "\n2. Retrieve Person Age Between 13 To 18");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    RetrievePersonAgeLessThan60(list);
+                    break;
+                case 2:
+                    RetrievePersonAgeBetween13To18(list);
+                    break;
+                default:
+                    Console.WriteLine("Please choose correct option");
+                    break;
+            }
             Console.ReadLine();
         }
         //UC1 - Add default values to list
@@ -26,7 +40,7 @@ namespace PersonDataManagement
             list.Add(new Person() { ssn = 3, name = "Virat", address = "Karnataka", age = 70 });
             list.Add(new Person() { ssn = 4, name = "Rohit", address = "Maharastra", age = 50 });
             list.Add(new Person() { ssn = 5, name = "Sachin", address = "Maharastra", age = 61 });
-            list.Add(new Person() { ssn = 6, name = "Pandya", address = "Maharastra", age = 35 });
+            list.Add(new Person() { ssn = 6, name = "Pandya", address = "Maharastra", age = 16 });
             list.Add(new Person() { ssn = 1, name = "Rahul", address = "Karnataka", age = 63 });
             IterateOverList(list);
         }
@@ -38,12 +52,19 @@ namespace PersonDataManagement
                 Console.WriteLine(person);
             }
         }
-        //Method to Retrieve top 2 Person Age Less Than 60 from list
+        //UC2 - Method to Retrieve top 2 Person Age Less Than 60 from list
         public static void RetrievePersonAgeLessThan60(List<Person> list)
         {
-            List<Person> topTwoRecords = list.FindAll(p => p.age < 60).OrderBy(a=>a.age).Take(2).ToList();
+            List<Person> topTwoRecords = list.FindAll(person => person.age < 60).OrderBy(a=>a.age).Take(2).ToList();
             Console.WriteLine("\nTop 2 persons with Age less than 60");
             IterateOverList(topTwoRecords);
+        }
+        //UC3 - Method to Retrieve all records from the list for Age between 13 to 18
+        public static void RetrievePersonAgeBetween13To18(List<Person> list)
+        {
+            List<Person> teenage = list.FindAll(person => person.age >=13 && person.age <=18).ToList();
+            Console.WriteLine("\nlist for Age between 13 to 18");
+            IterateOverList(teenage);
         }
     }
 }
