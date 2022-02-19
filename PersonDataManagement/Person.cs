b5c1bb2567fd75ff446e3d96e22b5f46543555ac
Guruprasad489 <guruprasadk.dev@gmail.com>
2022-02-20 00:15:17 +0530

    [Guru] Added a method RetrievePersonAgeLessThan60 using lambda expressions -UC2

--- PersonDataManagement/Person.cs
+++ PersonDataManagement/Person.cs
@@ -12,5 +12,9 @@ namespace PersonDataManagement
         public string name { get; set; }
         public string address { get; set; }
         public int age { get; set; }
+        public override string ToString()
+        {
+            return $"SSN: {ssn}, Name: {name}, Address: {address}, Age: {age}";
+        }
     }
 }
--- PersonDataManagement/Program.cs
+++ PersonDataManagement/Program.cs
@@ -13,8 +13,11 @@ namespace PersonDataManagement
             Console.WriteLine("Welcome to Person data management program");
             List<Person> list = new List<Person>();
             AddPersonDetailsToList(list);
+            RetrievePersonAgeLessThan60(list);
+
             Console.ReadLine();
         }
+        //UC1 - Add default values to list
         public static void AddPersonDetailsToList(List<Person> list)
         {
             // Object Initializer
@@ -25,6 +28,22 @@ namespace PersonDataManagement
             list.Add(new Person() { ssn = 5, name = "Sachin", address = "Maharastra", age = 61 });
             list.Add(new Person() { ssn = 6, name = "Pandya", address = "Maharastra", age = 35 });
             list.Add(new Person() { ssn = 1, name = "Rahul", address = "Karnataka", age = 63 });
+            IterateOverList(list);
+        }
+        //Method to Iterate over list
+        public static void IterateOverList(List<Person> list)
+        {
+            foreach (Person person in list)
+            {
+                Console.WriteLine(person);
+            }
+        }
+        //Method to Retrieve top 2 Person Age Less Than 60 from list
+        public static void RetrievePersonAgeLessThan60(List<Person> list)
+        {
+            List<Person> topTwoRecords = list.FindAll(p => p.age < 60).OrderBy(a=>a.age).Take(2).ToList();
+            Console.WriteLine("\nTop 2 persons with Age less than 60");
+            IterateOverList(topTwoRecords);
         }
     }
 }
